53dfbb3637daead958b9aa2691dc6435427e0d3a
kishanp1 <kishanpatel.ks1995@gmail.com>
2019-03-25 22:18:41 -0400

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'encryptEnv';
+  encrypted = '';
+  constructor(private http:HttpClient) {
+    }
+  encryptPassword(form: NgForm) {
+   console.log("YEEES");
+   console.log(form.value.password);
+   console.log(form.value.env);
+   let header = new HttpHeaders();
+   let body = new HttpParams();
+   body = body.append("password",form.value.password);
+   body = body.append("environment",form.value.env);
+   console.log(body);
+
+   this.http.get('https://api.github.com/users/gunjank').subscribe(response => console.log(response));
+   this.encrypted='done';
+ }
+ onSubmit(f: NgForm) {
+}
 }
